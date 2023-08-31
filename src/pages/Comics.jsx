3616e1fb674131d40142e6caddd31fa49bf41c16
jdev92 <juliencavaillon6@gmail.com>
2023-08-31 10:07:52 +0200

    update comics

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Comics = () => {
-  return <div>Comics</div>;
+  const [comics, setComics] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/comics");
+        setComics(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Comics</h1>
+      {comics.map(comics => (
+        <div key={comics._id}>
+          <h2>{comics.title} </h2>
+          <p>{comics.description} </p>
+          <img
+            src={`${comics.thumbnail.path}/portrait_uncanny.${comics.thumbnail.extension}`}
+            alt=""
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Comics;
