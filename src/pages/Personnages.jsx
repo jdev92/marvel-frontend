cb8b4b60ae36d38be95c3dad64e0396fb93b92b5
jdev92 <juliencavaillon6@gmail.com>
2023-08-31 20:07:57 +0200

    update Personnages

--- src/pages/Personnages.jsx
+++ src/pages/Personnages.jsx
@@ -20,30 +20,36 @@ const Personnages = () => {
   }, [search]); // Utiliser une dépendance vide pour exécuter cette requête une seule fois
 
   return (
-    <div>
-      <div className="search">
-        <input
-          type="text"
-          placeholder="Rechercher un personnage"
-          value={search}
-          onChange={event => {
-            setSearch(event.target.value);
-          }}
-        />
-      </div>
-      <h1>Personnages</h1>
-      {personnages.map(personnage => (
-        <article key={personnage._id}>
-          <h2>{personnage.name}</h2>
-          <p>{personnage.description}</p>
-          <Link to={`/character/${personnage._id}`}>
-            <img
-              src={`${personnage.thumbnail.path}/portrait_uncanny.${personnage.thumbnail.extension}`}
-              alt={personnage.name}
+    <div className="container">
+      <main>
+        <div>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Rechercher un personnage"
+              value={search}
+              onChange={event => {
+                setSearch(event.target.value);
+              }}
             />
-          </Link>
-        </article>
-      ))}
+          </div>
+          <div className="card-character">
+            <h1>Personnages</h1>
+            {personnages.map(personnage => (
+              <article key={personnage._id}>
+                <h2>{personnage.name}</h2>
+                <Link to={`/character/${personnage._id}`}>
+                  <img
+                    src={`${personnage.thumbnail.path}/portrait_uncanny	.${personnage.thumbnail.extension}`}
+                    alt={personnage.name}
+                  />
+                </Link>
+                <p>{personnage.description}</p>
+              </article>
+            ))}
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
