bd36a21037eb4a701aeddfc994cf8e690c7bc559
jdev92 <juliencavaillon6@gmail.com>
2023-08-31 20:07:17 +0200

    update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
-import logoMarvel from "../assets/marvel-logo.jpg";
+import logoMarvel from "../assets/marvel-logo.png";
+import img from "../assets/marvel-background.jpg";
 
 const Header = () => {
   return (
     <>
       <header>
-        <div className="header-logo">
-          <Link to="/">
-            <img src={logoMarvel} alt="" />
-          </Link>
-        </div>
-        <div className="header-link">
-          <Link to="/characters">
-            <button>Personnages</button>
-          </Link>
-          <Link to="/comics">
-            <button>Comics</button>
-          </Link>
-          <Link to="/favoris">
-            <button>Favoris</button>
-          </Link>
+        <div className="header">
+          <div className="header-logo">
+            <Link to="/">
+              <img src={logoMarvel} alt="logo marvel" />
+            </Link>
+          </div>
+          <div className="header-link">
+            <Link to="/characters">
+              <button>Personnages</button>
+            </Link>
+            <Link to="/comics">
+              <button>Comics</button>
+            </Link>
+            <Link to="/favoris">
+              <button>Favoris</button>
+            </Link>
+          </div>
         </div>
       </header>
     </>
